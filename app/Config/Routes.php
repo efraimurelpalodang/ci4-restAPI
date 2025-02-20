7ddff581d23216c16f8c6c606838a2a4ca65b310
efraim urel <efraimurelpalodang@gmail.com>
2025-02-20 09:51:29 +0800

    berhasil membuat api untuk created data

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,3 +7,4 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 $routes->get('/pegawai', 'PegawaiController::index');
+$routes->post('/pegawai', 'PegawaiController::create');
